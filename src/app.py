f7998ddbf715dd3d9aa5e21988ab821b5f98a65d
Daniel <danersow@gmail.com>
2020-10-25 00:00:52 +0300

    asdas

--- src/app.py
+++ src/app.py
@@ -1090,8 +1090,8 @@ def main():
     updater.start_webhook(listen='0.0.0.0',
                       port=8443,
                       url_path='TOKEN',
-                      key='/etc/ssl/new/private.key',
-                      cert='/etc/ssl/new/cert.pem',
+                      key='/etc/ssl/private/private.key',
+                      cert='/etc/ssl/certificate.crt',
                       webhook_url=f"https://danielkanzel.xyz:8443/{TOKEN}")
 
     print("================================================== ВЕБХУКИ ВСТАЛИ")
